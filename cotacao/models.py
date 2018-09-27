ad5dbe28259d1624e02ca5bf23df60f4cc7b63ae
Tiago Miguel <tmcstiago@gmail.com>
2018-09-27 01:37:42 -0300

    Finalizando os models para o formulario

--- cotacao/models.py
+++ cotacao/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Telefone(models.Model):
     numero = models.CharField(max_length=11)
-    tipo = models.CharField(max_length=20)
 
     def __str__(self):
         return self.numero
@@ -18,8 +17,9 @@ class Email(models.Model):
 
 class Segurado(models.Model):
     cpf = models.CharField(max_length=14)
-    telefones = models.ManyToManyField(Telefone, blank=True, null=True)
-    emails = models.ManyToManyField(Email, blank=True, null=True)
+    pessoa_fisica = models.BooleanField()
+    telefones = models.ManyToManyField(Telefone, verbose_name="Telefone", blank=True, null=True)
+    emails = models.ManyToManyField(Email, verbose_name="Email", blank=True, null=True)
 
     def __str__(self):
         return str(self.cpf) + " - " + str(self.seguradodados_set.last())
@@ -28,7 +28,6 @@ class Segurado(models.Model):
 # demais informações do Segurado
 class SeguradoDados(models.Model):
     nome = models.CharField(max_length=100)
-    pessoa_fisica = models.BooleanField()
     cpf = models.ForeignKey(
         Segurado,
         on_delete=models.PROTECT,
@@ -38,7 +37,7 @@ class SeguradoDados(models.Model):
     data_nascimento = models.DateField()
 
     def __str__(self):
-        return self.nome
+        return self.nome + " - " + str(self.cpf.cpf)
 
 
 # quem está utilizando a plataforma para fazer cotação
@@ -55,19 +54,81 @@ class Usuario(models.Model):
         return str(self.id) + " - " + self.nome
 
 
+class Seguro(models.Model):
+    renovacao = models.BooleanField()
+    inicio_vigencia = models.DateField()
+    termino_vigencia = models.DateField()
+    seguradora = models.CharField(max_length=50, blank=True, null=True)
+    apolice = models.CharField(max_length=20, blank=True, null=True)
+    bonus = models.PositiveIntegerField(blank=True, null=True)
+    qnt_sinistro = models.PositiveIntegerField(verbose_name="Quantidade de Sinistro", blank=True, null=True)
+
+
+class PrincipalCondutor(models.Model):
+    relacao = models.CharField(max_length=15)
+    cpf = models.CharField(max_length=11)
+    nome = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=1)
+    estado_civil = models.CharField(max_length=15)
+    residencia = models.CharField(max_length=30)
+    profissao = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
+class QAR (models.Model):
+    cep = models.CharField(max_length=8, verbose_name="CEP")
+    garagem_residencia = models.CharField(max_length=50)
+    garagem_trabalho = models.CharField(max_length=50)
+    garagem_curso = models.CharField(max_length=50)
+    utilizacao_veiculo = models.CharField(max_length=20)
+    condutores_25anos = models.BooleanField()
+    condutores_25anos_sexo = models.CharField(max_length=10)
+    distancia_trabalho = models.PositiveIntegerField()
+    km_mensal = models.PositiveIntegerField()
+    vitima_roubo = models.BooleanField(verbose_name="Vitima de Roubo")
+
+    def __str__(self):
+        return str(self.veiculo_set)
+
+
 class Veiculo(models.Model):
-    cotacao = models.OneToOneField(
+    segurado = models.ForeignKey(
         Segurado,
         verbose_name="id da cotacao",
-        on_delete=models.PROTECT)
+        on_delete=models.PROTECT
+    )
+    seguro = models.OneToOneField(
+        Seguro,
+        on_delete=models.PROTECT
+    )
+    principal_condutor = models.OneToOneField(
+        PrincipalCondutor,
+        on_delete=models.PROTECT,
+        blank=True, null=True
+    )
+    qar = models.OneToOneField(
+        QAR,
+        on_delete=models.PROTECT,
+        verbose_name="QAR",
+        blank=True, null=True
+    )
     chassi = models.CharField(max_length=17, blank=True, null=True)
     placa = models.CharField(max_length=7, blank=True, null=True)
-    marca = models.CharField(max_length=20, blank=True, null=True)
-    modelo = models.CharField(max_length=100, blank=True, null=True)
+    marca = models.CharField(max_length=20)
+    modelo = models.CharField(max_length=100)
+    combustivel = models.CharField(max_length=20)
     ano_modelo = models.PositiveIntegerField()
     ano_fabricacao = models.PositiveIntegerField()
     zero_km = models.BooleanField()
-
+    alienacao = models.BooleanField()
+    chassi_remarcado = models.BooleanField()
+    blindagem = models.BooleanField()
+    tipo_veiculo = models.CharField(max_length=20)
+    isencao_fiscal = models.CharField(max_length=20)
+    cat_anti_furto = models.CharField(max_length=50, verbose_name="Categoria Anti-furto")
+    marca_anti_furto = models.CharField(max_length=50)
 
     def __str__(self):
         return self.modelo
